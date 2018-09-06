d379f2af2af2f98c78e7b41bd0797dce1af9cf90
Rigoberto Saenz Imbacuan <beto456789@gmail.com>
2018-09-06 13:27:34 -0500

    All required endpoints added, they return hardcoded json responses

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -7,7 +7,93 @@ const functions = require("firebase-functions");
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase! v2");
+exports.ping = functions.https.onRequest((request, response) => {
+    response.send("Awesome City Trips firebase project is working!");
+});
+/*
+IN:
+
+OUT:
+    Categories []
+*/
+exports.getCategories = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\",\"categories\":[{\"id\":0,\"name\":\"Business\",\"banner_url\":\"https://travel.state.gov/content/travel/en/us-visas/business/_jcr_content/tsg-rwd-content-page-parsysxxx/slideshow.img.png/1512148994695.png\",\"description\":\"Business: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":1,\"name\":\"Gastronomy\",\"banner_url\":\"https://www.visitdenmark.com/sites/default/files/styles/news_and_article_image/public/vdk_images/Travel-trade/top-tema-gastronomy.png?itok=3AlhLjdw\",\"description\":\"Gastronomy: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":2,\"name\":\"History\",\"banner_url\":\"https://blog.my.com/wp-content/uploads/2016/11/Depositphotos_22942194_m-2015.jpg\",\"description\":\"History: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":3,\"name\":\"Nature\",\"banner_url\":\"http://croatia.hr/sites/default/files/styles/image_full_width/public/2017-08/02_01_slide_nature.jpg?itok=ItAHmLlp\",\"description\":\"Nature: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":4,\"name\":\"Party\",\"banner_url\":\"http://www.hollandevenementengroep.nl/foto/11902/1000/files/Accent%20Fotos/the_party.jpg\",\"description\":\"Party: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":5,\"name\":\"Study\",\"banner_url\":\"https://www.brainscape.com/blog/wp-content/uploads/2011/11/696dcdd6157d0438eed57eac9b6266b5.jpg\",\"description\":\"Study: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"},{\"id\":6,\"name\":\"Surgery\",\"banner_url\":\"https://thenypost.files.wordpress.com/2017/06/170629-young-women-plstic-surgery-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1\",\"description\":\"Surgery: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"}]}");
+});
+/*
+IN:
+    - CategoryID
+OUT:
+    - Events []
+*/
+exports.getEventsFromCategory = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - EventID
+OUT:
+    - Event
+*/
+exports.getEvent = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - UserID
+OUT:
+    - Credit Cards []
+*/
+exports.getCreditCardsFromUser = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - UserID
+    - CreditCardID
+    - EventID
+
+OUT:
+    - Status {success, error}
+*/
+exports.buyEvent = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - UserCoordinates
+    - NumberOfNearEvents
+OUT:
+    - Events []
+*/
+exports.getEventFromLocation = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - UserID
+OUT:
+    - Events []
+*/
+exports.getEventsBuyedByUser = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - UserID
+OUT:
+    - User
+*/
+exports.getUserInfo = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
+});
+/*
+IN:
+    - Username (Auth?)
+    - Password (Auth?)
+OUT:
+    UserID
+*/
+exports.getUserID = functions.https.onRequest((request, response) => {
+    response.send("{\"status\":\"success\"}");
 });
 //# sourceMappingURL=index.js.map
