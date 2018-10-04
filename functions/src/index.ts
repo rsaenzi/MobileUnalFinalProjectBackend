92b80f20b8a0f737cc926bc550be6c255c4b82b1
lemartinp <lemartinp@unal.edu.co>
2018-10-04 00:38:24 -0500

    endpoint getEvent() done, also added failed status to getEventsFromCategory()

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -49,21 +49,25 @@ Out:
 export const getEventsFromCategory = functions.https.onRequest((request, response) => {
 	console.log(request.params["0"]);
 	var parametro = String(request.params["0"]);
-	parametro = parametro.replace("/","");
-	var parametro2 = parseInt(parametro);
-	console.log(parametro);
-	var events = firebase.database().ref('events');
-	events.once('value').then(function(snap) {
-		//response.status(200).json({status:"success",categories: snap.val()});
-		var hola = [];
-		snap.forEach(function (childSnapshot) {
-		var childObject = childSnapshot.val();
-		  if (childObject.category_id == parametro2){
-			  hola.push(childObject);
-		  }
-        });
-		response.status(200).json({status:"success",events: hola});
-	});
+  if(parametro == "")
+    response.status(200).json({status:"failed"});
+  else{
+  	parametro = parametro.replace("/","");
+  	var parametro2 = parseInt(parametro);
+  	console.log(parametro);
+  	var events = firebase.database().ref('events');
+  	events.once('value').then(function(snap) {
+  		//response.status(200).json({status:"success",categories: snap.val()});
+  		var hola = [];
+  		snap.forEach(function (childSnapshot) {
+  		var childObject = childSnapshot.val();
+  		  if (childObject.category_id == parametro2){
+  			  hola.push(childObject);
+  		  }
+          });
+  		response.status(200).json({status:"success",events: hola});
+    });
+  }
     //response.send("{\"status\":\"success\",\"events\":[{\"id\":0,\"category_id\":0,\"name\":\"Campus Party Colombia\",\"description\":\"Campus Party Colombia: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\",\"icon_url\":\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"gallery_urls\":[\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\"],\"place\":{\"id\":0,\"name\":\"Corferias\",\"coordinates\":{\"longitude\":123.123,\"latitude\":987.987}},\"total_seats\":200,\"available_seats\":150,\"rating\":3,\"date\":\"2018-05-03T09:30:00.000Z\",\"price\":150000,\"organizer\":{\"id\":0,\"name\":\"Telefonica\"}},{\"id\":1,\"category_id\":0,\"name\":\"Alimentarte\",\"description\":\"Alimentarte: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\",\"icon_url\":\"https://pbs.twimg.com/profile_images/693113926989549569/iPDkmKNF_400x400.png\",\"gallery_urls\":[\"https://pbs.twimg.com/profile_images/693113926989549569/iPDkmKNF_400x400.png\",\"https://pbs.twimg.com/profile_images/693113926989549569/iPDkmKNF_400x400.png\",\"https://pbs.twimg.com/profile_images/693113926989549569/iPDkmKNF_400x400.png\"],\"place\":{\"id\":1,\"name\":\"Parque el Virrey\",\"coordinates\":{\"longitude\":123.123,\"latitude\":987.987}},\"total_seats\":500,\"available_seats\":30,\"rating\":5,\"date\":\"2018-10-05T11:45:00.000Z\",\"price\":25000,\"organizer\":{\"id\":1,\"name\":\"Alimentarte Food Festival SAS\"}}]}");
 });
 
@@ -76,10 +80,34 @@ Out:
     status
     event
 */
+/*
 export const getEvent = functions.https.onRequest((request, response) => {
     response.send("{\"status\":\"success\",\"event\":[{\"id\":0,\"category_id\":0,\"name\":\"Campus Party Colombia\",\"description\":\"Campus Party Colombia: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\",\"icon_url\":\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"gallery_urls\":[\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\",\"https://pbs.twimg.com/profile_images/943783830611222528/KiHx6Mh6_400x400.jpg\"],\"place\":{\"id\":0,\"name\":\"Corferias\",\"coordinates\":{\"longitude\":123.123,\"latitude\":987.987}},\"total_seats\":200,\"available_seats\":150,\"rating\":3,\"date\":\"2018-05-03T09:30:00.000Z\",\"price\":150000,\"organizer\":{\"id\":0,\"name\":\"Telefonica\"}}]}");
 });
+*/
 
+export const getEvent = functions.https.onRequest((request, response) => {
+    var parametro = String(request.params["0"]);
+    parametro = parametro.replace("/","");
+    var parametro2 = parseInt(parametro);
+    console.log(parametro2);
+    var evento = firebase.database().ref('events');
+	  evento.once('value').then(function(snap) {
+      var salida;
+      var succeded = false;
+  		snap.forEach(function (childSnapshot) {
+  		var childObject = childSnapshot.val();
+  		  if (childObject.id == parametro2){
+          succeded = true;
+          salida = childObject;
+  		  }
+      });
+      if(succeded)
+  		  response.status(200).json({status:"success",event: salida});
+      else
+        response.status(200).json({status:"failed"});
+	});
+});
 /*
 Endpoint: getCreditCardsFromUser
 
